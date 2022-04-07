511fec9d5edb70d9a5963cc7bca66b100e47f883
salmaep <95133669+salmaep@users.noreply.github.com>
2022-04-07 08:14:48 +0700

    add tugas prak minggu 7

---
+++ Praktikum Minggu ke-7/ATMMain.java
@@ -0,0 +1,9 @@
+package ATMProgram; 
+
+public class ATMMain {
+   // main method creates and runs the ATM
+   public static void main(String[] args) {
+      ATM theATM = new ATM();    
+      theATM.run();
+   }
+} 
