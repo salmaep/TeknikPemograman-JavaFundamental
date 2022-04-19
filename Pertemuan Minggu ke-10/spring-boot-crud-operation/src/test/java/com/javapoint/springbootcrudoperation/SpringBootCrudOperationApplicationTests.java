1608af67466c865fcf1d3db331dc058ff6349263
salmaep <95133669+salmaep@users.noreply.github.com>
2022-04-19 22:39:59 +0700

    add data tugas

---
+++ Pertemuan Minggu ke-10/spring-boot-crud-operation/src/test/java/com/javapoint/springbootcrudoperation/SpringBootCrudOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javapoint.springbootcrudoperation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
